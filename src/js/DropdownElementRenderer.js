b51aa75fdd6ad4f1ee5530521f6c2f8c0daf8b3f
Dmitry R <dimsog@gmail.com>
2020-10-17 16:06:29 +0300

    Add renderer function

--- src/js/DropdownElementRenderer.js
+++ src/js/DropdownElementRenderer.js
@@ -1,23 +1,31 @@
 export default class DropdownElementRenderer {
     render(option) {
         if (option.items === undefined || option.items.length === 0) {
-
+            return this._$renderOption(option);
         } else {
-
+            return this._$renderOptionGroup(option);
         }
+    }
+
+    _$renderOptionGroup(option) {
+        const $node = document.createElement('li');
+        $node.classList.add('dropdownselect--optiongroup');
+        const $groupName = document.createElement('span');
+        $groupName.innerHTML = option.name;
+        const $container = document.createElement('ul');
+        for (const item of option.items) {
+            $container.append(this._$renderOption(item));
+        }
+        $node.appendChild($groupName);
+        $node.appendChild($container);
+        return $node;
+    }
+
+    _$renderOption(option) {
         const $node = document.createElement('li');
         const $optionNode = document.createElement('span');
         $optionNode.innerHTML = option.name;
         $node.appendChild($optionNode);
-        if (option.items !== undefined) {
-            for (const subOption of option.items) {
-
-            }
-        }
-        this.$dropdown.appendChild(option.$node);
-    }
-
-    _$renderOptGroup(name) {
-
+        return $node;
     }
 }
